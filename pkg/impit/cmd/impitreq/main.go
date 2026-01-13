5952103a2247b2b7591120db1c1920c1fd93d5df
wenggo <wengo@ierpsys.com>
2026-01-13 17:33:44 +0800

    fix(cgo): remove duplicate exported symbols from main.go; rely on shared.go (cshared) for C API

--- pkg/impit/cmd/impitreq/main.go
+++ pkg/impit/cmd/impitreq/main.go
@@ -1,24 +1,3 @@
 package main
 
-/*
-#include <stdlib.h>
-*/
-import "C"
-import (
-	"strconv"
-	"unsafe"
-)
-
-//export ImpitHandleRequestJSON
-func ImpitHandleRequestJSON(req *C.char) *C.char {
-	in := C.GoString(req)
-	out := `{"ok":true,"echo":` + strconv.Quote(in) + `}`
-	return C.CString(out)
-}
-
-//export ImpitFree
-func ImpitFree(p *C.char) {
-	C.free(unsafe.Pointer(p))
-}
-
 func main() {}
